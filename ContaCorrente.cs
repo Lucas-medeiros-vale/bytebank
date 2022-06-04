8a785dd409128f106e532148b5f34ad464845af7
Lucascyber-97 <lucasdovale29@gmail.com>
2022-06-03 23:27:54 -0300

    bytebank

---
+++ ContaCorrente.cs
@@ -0,0 +1,127 @@
+﻿using bytebank.Titular;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {      
+        public Cliente Titular { get; set; }
+
+        private string _conta;
+        public string Conta { 
+            get
+            {
+                return _conta;
+            }
+            set
+            {
+                if(value == null)
+                {
+                    return;
+                }
+                else
+                {
+                    _conta = value;
+                }
+            }
+        
+        }
+
+        private int _numero_agencia; 
+        public int Numero_agencia {
+            get
+            {
+                return _numero_agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+
+                }
+                else
+                {
+                    _numero_agencia = value;
+                }
+            }
+        
+        }
+
+        public string Nome_agencia { get; set; }
+
+        private double saldo;
+
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                saldo = value;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                return true;
+            }
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo = saldo + valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            if (valor < 0 )
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+
+
+        public ContaCorrente(int numero_agencia, string conta)
+        {
+            Numero_agencia = numero_agencia;
+            Conta = conta;
+            TotalDeContasCriadas += 1;
+        }
+        //A palavra reservada static é utilizada para definir um campo ou propriedade como membro de uma classe
+        public static int TotalDeContasCriadas { get; set; }
+
+        public ContaCorrente(Cliente titular, string nome_agencia, int numero_agencia, string conta) 
+        {
+            Titular = titular;
+            Nome_agencia = nome_agencia;
+            Numero_agencia = numero_agencia;
+            Conta = conta;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using bytebank;
+using bytebank.Titular;
+
+Console.WriteLine("Boas Vindas ao seu Banco, ByteBank!\n");
+
+//ContaCorrente conta1 = new ContaCorrente();
+//conta1.titular = "Lucas Medeiros";
+//conta1.conta = "10123-X";
+//conta1.numero_agencia = 23;
+//conta1.nome_agencia = "Agência Central";
+//conta1.saldo = 100;
+
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titular = "Amanda Nogueira";
+//conta2.conta = "111999-X";
+//conta2.numero_agencia = 58;
+//conta2.nome_agencia = "Agência Central";
+//conta2.saldo = 100;
+
+//// Efetuando Transferência...
+//Console.WriteLine("Saldo da Amanda pré-transferência: " + conta2.saldo);
+//Console.WriteLine("Saldo do Lucas pré-transferência: " + conta1.saldo);
+
+//bool transferencia = conta1.Transferir(50, conta2);
+//Console.WriteLine("Transferência realizada com sucesso? " +transferencia);
+
+//Console.WriteLine("Saldo da Amanda pós-transferência: " + conta2.saldo);
+//Console.WriteLine("Saldo do Lucas pós-transferência: " + conta1.saldo);
+
+//// Exibindo Dados da Conta...
+//conta1.ExibirDadosDaConta();
+
+//// Criando um objeto cliente:
+//Cliente cliente = new Cliente();
+//cliente.nome = "Lucas Medeiros";
+//cliente.cpf = "123.456.789-10";
+//cliente.profissao = "Programador";
+
+//ContaCorrente conta3 = new ContaCorrente();
+//conta3.titular = new Cliente();
+//conta3.titular.nome = "Lucas Medeiros";
+//conta3.titular.cpf = "123.456.789-10";
+//conta3.titular.profissao = "Programador";
+//conta3.conta = "1234567-X";
+//conta3.numero_agencia = 35;
+//conta3.nome_agencia = "Agência Central";
+
+//Console.WriteLine(conta3.titular.nome);
+
+//if(conta3.titular==null)
+//{
+//    Console.WriteLine("O campo titular está nulo!");
+//}
+
+//Cliente sarah = new Cliente();
+//sarah.Nome = "Sarah Silva";
+//sarah.Profissao = "Professora";
+//sarah.Cpf = "111111111-12";
+
+//Cliente ester = new Cliente();
+//ester.Nome = "Ester Almeida";
+//ester.Profissao = "Advogada";
+//ester.Cpf = "868524125-32";
+
+//Console.WriteLine("Total de Clientes: " + Cliente.TotalClienteCadastrados);
+
+//ContaCorrente conta4 = new ContaCorrente(235, "123456-X");
+//conta4.Saldo = 100;
+//conta4.Titular = sarah;
+//Console.WriteLine(conta4.Titular.Nome);
+//Console.WriteLine(conta4.Saldo);
+//Console.WriteLine(conta4.Numero_agencia);
+//Console.WriteLine(conta4.Conta);
+
+
+
+
+
+
+
+Console.ReadKey();
